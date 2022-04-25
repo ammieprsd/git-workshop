8510f6e885cf47062e873a5348331aec8684edc7
ammieprsd <ammieprsd@gmail.com>
2022-04-25 16:45:53 +0200

    fields en constructor aangemaakt

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,6 +1,13 @@
 package controller;
 
 public class UserController {
+    private String name;
+    private int age;
+    private String firstname;
 
-
+    public UserController(String name, int age, String firstname) {
+        this.name = name;
+        this.age = age;
+        this.firstname = firstname;
+    }
 }
